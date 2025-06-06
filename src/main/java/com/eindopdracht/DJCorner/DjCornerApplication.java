d3e230b0cd9c3d37133c542666a7e99629f8887b
DanielSeton <danielseton@live.nl>
2025-06-06 16:50:27 +0200

    Project opgezet

---
+++ src/main/java/com/eindopdracht/DJCorner/DjCornerApplication.java
@@ -0,0 +1,13 @@
+package com.eindopdracht.DJCorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjCornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjCornerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eindopdracht/DJCorner/DjCornerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eindopdracht.DJCorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjCornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
