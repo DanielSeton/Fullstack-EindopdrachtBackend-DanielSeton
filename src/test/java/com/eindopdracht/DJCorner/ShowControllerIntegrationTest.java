dadea81e8fbed08244248189fc19c3e820a3f8ab
DanielSeton <danielseton@live.nl>
2025-09-02 16:51:25 +0200

    Integratie test afgerond en getest

--- src/test/java/com/eindopdracht/DJCorner/ShowControllerIntegrationTest.java
+++ src/test/java/com/eindopdracht/DJCorner/ShowControllerIntegrationTest.java
@@ -1,5 +1,9 @@
 package com.eindopdracht.DJCorner;
 
+import com.eindopdracht.DJCorner.models.Show;
+import com.eindopdracht.DJCorner.repositories.ShowRepository;
+import com.eindopdracht.DJCorner.services.ShowService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +11,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.matchesPattern;
+import java.time.LocalDate;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @SpringBootTest
@@ -23,6 +26,39 @@ public class ShowControllerIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    private ShowService showService;
+
+    @Autowired
+    private ShowRepository showRepository;
+
+    private Show show1;
+    private Show show2;
+
+    @BeforeEach
+    void setUp() {
+        if(showRepository.count()>0) {
+            showRepository.deleteAll();
+        }
+
+        show1 = new Show();
+        show1.setName("Test Festival");
+        show1.setLocation("Amsterdam, Netherlands");
+        show1.setDate(LocalDate.of(2025, 10, 10));
+        show1.setWebsite("https://testfestival.com");
+        show1.setTicketSite("https://tickets.testfestival.com");
+
+        show2 = new Show();
+        show2.setName("Hardcore Fest");
+        show2.setLocation("Rotterdam, Netherlands");
+        show2.setDate(LocalDate.of(2025, 10, 10));
+        show2.setWebsite("https://hardcorefest.com");
+        show2.setTicketSite("https://tickets.hardcorefest.com");
+
+        showRepository.save(show1);
+        showRepository.save(show2);
+    }
 
     @Test
     @DisplayName("Should create new Show")
@@ -38,16 +74,36 @@ public class ShowControllerIntegrationTest {
                 }
                 """;
 
-        MvcResult result = this.mockMvc
-                .perform(MockMvcRequestBuilders.post("/show")
-                .contentType(APPLICATION_JSON)
-                .content(requestJson))
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post("/shows")
+                    .contentType(APPLICATION_JSON)
+                    .content(requestJson))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andReturn();
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Mega Piraten Festijn"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.location").value("Arnhem, Netherlands"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.date").value("2025-12-20"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.website").value("https://www.novi.nl/"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.ticketSite").value("https://www.novi.nl/"));
+    }
 
-        String createdId = result.getResponse().getContentAsString();
-
-        assertThat(result.getResponse().getHeader("Location"), matchesPattern("^.*/show/" + createdId));
+    @Test
+    @DisplayName("Should get all Shows")
+    public void getShows() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/shows"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(show1.getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Test Festival"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].location").value("Amsterdam, Netherlands"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].date").value("2025-10-10"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].website").value("https://testfestival.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].ticketSite").value("https://tickets.testfestival.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(show2.getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("Hardcore Fest"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].location").value("Rotterdam, Netherlands"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].date").value("2025-10-10"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].website").value("https://hardcorefest.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].ticketSite").value("https://tickets.hardcorefest.com"));
     }
 }
